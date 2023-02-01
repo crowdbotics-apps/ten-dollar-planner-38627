02f46d662fac550759df7b450c573ccba6cbae4a
Crowdbotics <team@crowdbotics.com>
2023-02-01 08:19:50 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "ten-dollar-planner-38627",
+  url: "https://ten-dollar-planner-38627.botics.co",
+  api: "https://ten-dollar-planner-38627.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
